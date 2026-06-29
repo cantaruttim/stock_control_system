8d05270cd4c8aec08a4e8ce4ba1d008f60df9417
cantaruttim <cantaruttim@outlook.com>
2026-06-29 19:07:41 -0300

    enum products type

--- smart_control/src/main/java/br/com/stock_control/smart_control/model/enums/ProductType.java
+++ smart_control/src/main/java/br/com/stock_control/smart_control/model/enums/ProductType.java
@@ -1,5 +1,19 @@
 package br.com.stock_control.smart_control.model.enums;
 
 public enum ProductType {
-    
+    CANDY,
+    MEDICINES,
+    ELETRONICS,
+    CLOTHING,
+    FURNITURE,
+    SPORTS,
+    TOYS,
+    BOOKS,
+    BEAUTY,
+    AUTOMOTIVE,
+    JEWELRY,
+    PET_SUPPLIES,
+    MIXED_TYPE,
+    ONLINE_STORE,
+    OTHER
 }
