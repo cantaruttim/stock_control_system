9dc02cdc7a257776df96ade7b8b5b9758ed32ebd
Matheus de Almeida Cantarutti <cantaruttim@Matheuss-MacBook-Air.local>
2026-06-29 18:52:04 -0300

    Products info

--- smart_control/src/main/java/br/com/stock_control/smart_control/model/Client.java
+++ smart_control/src/main/java/br/com/stock_control/smart_control/model/Client.java
@@ -1,5 +1,7 @@
 package br.com.stock_control.smart_control.model;
 
 public class Client {
+
+    
     
 }
--- smart_control/src/main/java/br/com/stock_control/smart_control/model/Product.java
+++ smart_control/src/main/java/br/com/stock_control/smart_control/model/Product.java
@@ -2,4 +2,81 @@ package br.com.stock_control.smart_control.model;
 
 public class Product {
     
+    private String productSKU;
+    private String productName;
+    private String productDescription;
+    private String productBrand;
+    private double productPrice;
+    private int productQuantity;
+    
+
+    public Product() {}
+
+    public Product(
+        String productSKU, 
+        String productName, 
+        String productDescription, 
+        String productBrand, 
+        double productPrice, 
+        int productQuantity) {
+            this.productSKU = productSKU;
+            this.productName = productName;
+            this.productDescription = productDescription;
+            this.productBrand = productBrand;
+            this.productPrice = productPrice;
+            this.productQuantity = productQuantity;
+    }
+
+    public String getProductSKU() {
+        return productSKU;
+    }
+
+    public void setProductSKU(String productSKU) {
+        this.productSKU = productSKU;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getProductBrand() {
+        return productBrand;
+    }
+
+    public void setProductBrand(String productBrand) {
+        this.productBrand = productBrand;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(int productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+    public double calculateTotalValue() {
+        return productPrice * productQuantity;
+    }
+
 }
